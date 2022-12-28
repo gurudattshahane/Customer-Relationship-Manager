5bdfc3305e2d26dbf0c5029ee2588f05498e0964
Gurudatt Shahane <gurudattshahane@gmail.com>
2022-12-28 20:34:08 +0530

    Project created and added necessary dependencies in POM.xml

---
+++ src/test/java/com/mysite/CustomerRelationshipManager/CustomerRelationshipManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.CustomerRelationshipManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRelationshipManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
